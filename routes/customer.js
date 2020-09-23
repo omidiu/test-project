d6ff17e0d3c4f5e1c20fe1372bd0164f62836017
omid saghatchian <omidsaghatchian@gmail.com>
2020-09-23 07:05:46 +0330

    Fix indent

--- routes/customer.js
+++ routes/customer.js
@@ -6,11 +6,15 @@ const { customerController } = require('../controller/index');
 
 
 // Add customer
-router.post('/signup', customerValidator.isValidRegisterForm, customerController.create);
+router.post('/signup', 
+customerValidator.isValidRegisterForm, 
+customerController.create);
 
 
 // login customer
-router.post('/login', customerValidator.isValidloginForm, customerController.login);
+router.post('/login', 
+customerValidator.isValidloginForm, 
+customerController.login);
 
 
 // Get profile
@@ -20,44 +24,56 @@ customerController.getProfile)
 
 
 // Edit profile
-router.put('/profile', passport.authenticate('customer', {session: false}), customerValidator.isValidEditForm, customerController.editProfile);
+router.put('/profile', 
+passport.authenticate('customer', {session: false}), 
+customerValidator.isValidEditForm, 
+customerController.editProfile);
 
 
 // Add item to shopping cart
-router.post('/shopping-carts', passport.authenticate('customer', {session: false}), 
-customerValidator.isValidItem, customerController.addItemToShoppingCart);
+router.post('/shopping-carts', 
+passport.authenticate('customer', {session: false}), 
+customerValidator.isValidItem, 
+customerController.addItemToShoppingCart);
 
 
 // Get shopping cart items
-router.get('/shopping-carts', passport.authenticate('customer', {session: false}), 
+router.get('/shopping-carts', 
+passport.authenticate('customer', {session: false}), 
 customerController.getShoppingCart);
 
 
 // Edit shopping cart item
-router.put('/shopping-carts/:itemId', passport.authenticate('customer', {session: false}), 
-customerValidator.isValidEditItem, customerController.editItemOfShoppingCart);
+router.put('/shopping-carts/:itemId', 
+passport.authenticate('customer', {session: false}), 
+customerValidator.isValidEditItem, 
+customerController.editItemOfShoppingCart);
 
 
 
 // Delete shopping cart item
-router.delete('/shopping-carts/:itemId', passport.authenticate('customer', {session: false}), 
+router.delete('/shopping-carts/:itemId', 
+passport.authenticate('customer', {session: false}), 
 customerController.deleteItemOfShoppingCart);
 
 
 // Purchase shopping cart
-router.post('/purchase', passport.authenticate('customer', {session: false}),
+router.post('/purchase', 
+passport.authenticate('customer', {session: false}),
 customerValidator.isValidOrderShipping, 
 customerController.purchase);
 
 
 
 // Get all orders of customer
-router.get('/orders', passport.authenticate('customer', {session: false}), 
+router.get('/orders', 
+passport.authenticate('customer', {session: false}), 
 customerController.getAllOrders);
 
 
 // Get order of customer
-router.get('/orders/:orderId', passport.authenticate('customer', {session: false}), 
+router.get('/orders/:orderId', 
+passport.authenticate('customer', {session: false}), 
 customerController.getOrder);
 
 
